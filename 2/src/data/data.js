14c89594a7290441bc05edf17ef0cd218a0b88da
hjuarez-2019128 <125756031+hjuarez-2019128@users.noreply.github.com>
2024-10-05 11:10:04 -0600

    NECESITO EL PUSH

---
+++ 2/src/data/data.js
@@ -0,0 +1,91 @@
+const data = [
+	{
+		id: 1,
+		image:
+			'https://kinal.academy/pluginfile.php/404490/course/overviewfiles/Taller%20III%20bueno.gif',
+		title: 'Taller III: Dominando React y Node.js',
+		category: 'Tecnicas',
+		description: 'En nuestra clase de taller, pondremos en práctica el contenido aprendido. Creemos en aprender haciendo, por eso trabajarás en proyectos reales durante cada sesión. Esta experiencia práctica te preparará para los desafíos reales en programación',
+		date: 'Published on Ene 17, 2024',
+		ReadingTime: '5 min read',
+	},
+	{
+		id: 2,
+		image:
+			'https://kinal.academy/pluginfile.php/404491/course/overviewfiles/Pr%C3%A1ctica%20Supervisada%20AM.gif',
+		title: 'Práctica Supervisada: Afianzando tus Habilidades en Programación"',
+		category: 'Tecnicas',
+		description:
+			'En la práctica supervisada, combinamos tareas de taller con tecnología. Aplicamos todas las tecnologías aprendidas en proyectos prácticos. Esta experiencia integral fortalece tus habilidades en programación',
+		date: 'Published on Ene 14, 2024',
+		ReadingTime: '3 min read',
+	},
+	{
+		id: 3,
+		image:
+			'https://kinal.academy/pluginfile.php/404489/course/overviewfiles/Tecnolog%C3%ADa%20IIIAM.gif',
+		title: 'Tecnología: Innovando el Futuro, Transformando el Presente.',
+		category: 'Tecnicas',
+		description:
+			'En nuestro enfoque, comenzaremos explorando la teoría detrás de los proyectos, comprendiendo los principios fundamentales que los sustentan. Luego, nos sumergiremos en el mundo de las tecnologías actuales, aprendiendo herramientas y técnicas de vanguardia. ',
+		date: 'Published on Ene 19, 2024',
+		ReadingTime: '6 min read',
+	},
+	{
+		id: 4,
+		image:
+			'https://miro.medium.com/v2/resize:fit:1400/1*WqMpsn9c8JsbbIxIRr50ug.gif',
+		title: 'React: Innovando el Futuro del Desarrollo FrontEnd, Transformando la Experiencia del Usuario',
+		category: 'React',
+		description:
+			'Impulsando el Desarrollo FrontEnd para Crear Aplicaciones de SPA de Gran Escala. En la práctica supervisada, integramos tecnología con tareas prácticas, aplicando todas las herramientas aprendidas en proyectos reales. Esta experiencia inmersiva potencia tus habilidades en desarrollo.',
+		date: 'Published on Marz 06, 2024',
+		ReadingTime: '4 min read',
+	},
+	{
+		id: 5,
+		image:
+			'https://d585tldpucybw.cloudfront.net/docs/default-source/default-document-library/this-state.gif?sfvrsn=cf0a067f_0',
+		title: 'React: Proyectos Puros, Compromiso Actualizado',
+		category: 'React',
+		description:
+			'En la comunidad de React, abundan los proyectos de muestra que se centran en el uso exclusivo de React, sin bibliotecas de terceros para el manejo del estado. Nos comprometemos a mantener esta página enfocada en dichos proyectos, y te invitamos a unirte a este compromiso al agregar tu propio proyecto.',
+		date: 'Published on May 16, 2024',
+		ReadingTime: '10 min read',
+	},
+	{
+		id: 6,
+		image:
+			'https://s3-eu-west-1.amazonaws.com/blog.forestadmin.com/2021/09/FA-article-nodeJS@2x-1-.png',
+		title: 'Node.js: Proyectos Puros, Compromiso Actualizado',
+		category: 'Node.js',
+		description:
+			'Descubre una colección de proyectos exclusivos de Node.js, donde se enfatiza el uso nativo de esta poderosa plataforma sin la dependencia de bibliotecas externas para el manejo del estado. Mantenemos un compromiso constante de mantener estos proyectos al día con las últimas versiones de Node.js, asegurando así su relevancia y funcionalidad en un entorno en constante evolución. ',
+		date: 'Published on Feb 19, 2024',
+		ReadingTime: '8 min read',
+	},
+	{
+		id: 7,
+		image:
+			'https://www.weblineindia.com/wp-content/uploads/2023/10/NodeJS-for-Backend.jpg',
+		title: 'Node.js : Innovación Continua, Comunidad Activa',
+		category: 'Node.js',
+		description:
+			'Explora una variedad de proyectos backend desarrollados con Node.js, destacando su versatilidad y eficiencia en la construcción de aplicaciones escalables y robustas. Desde APIs hasta servidores web, estos proyectos demuestran el poder de Node.js en el manejo de la lógica del lado del servidor. ',
+		date: 'Published on Feb 20, 2024',
+		ReadingTime: '8 min read',
+	},
+	{
+		id: 8,
+		image:
+			'https://www.mongodb.com/community/forums/uploads/default/original/3X/1/d/1dfcf88a3e00f140834f58cc83f8164ea5913e8b.gif',
+		title: 'MongoDB: Explorando un Universo de Datos Flexibles y Escalables',
+		category: 'MongoDB',
+		description:
+			'MongoDB ofrece una plataforma versátil para gestionar grandes volúmenes de información. Únete a nuestra comunidad de desarrolladores y descubre cómo MongoDB está transformando la forma en que interactuamos con los datos, contribuyendo con tus propios proyectos para enriquecer aún más este emocionante ecosistema de bases de datos NoSQL.',
+		date: 'Published on Ene 21, 2024',
+		ReadingTime: '6 min read',
+	},
+];
+
+export default data;
